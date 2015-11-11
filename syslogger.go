88441531ababb82b988af605f9070d2460f65564
Stephen Price <steeef@gmail.com>
2015-11-11 10:29:57 -0800

    initial commit

---
+++ syslogger.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"log/syslog"
+	"os"
+)
+
+func main() {
+	tag := flag.String("tag", "go", "syslog tag or application name")
+	flag.Parse()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	logwriter, e := syslog.New(syslog.LOG_NOTICE|syslog.LOG_LOCAL4, *tag)
+	defer logwriter.Close()
+	if e == nil {
+		log.SetOutput(logwriter)
+	}
+
+	for scanner.Scan() {
+		log.Print(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
